27c413844f6fe34d8d2e6c13bdeef4902ea187e8
GlenReiseMalesa <malesaglen6@gmail.com>
2022-11-09 10:38:22 +0200

    added more routing functionality

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $router = new Router();
 $router->get('/','api/accommodation/accommodation.php');
 $router->get('/contacts','api/accommodation/contacts.php');
 $router->get('/addresses','api/accommodation/addresses.php');
-//$router->get('/accommodationbykey/$key','api/accommodation/accommodationbykey.php');
-//$router->get('/accommodationbyaddress/$address','api/accommodation/accommodationbylocation.php');
-//$router->get('/accommodationbypropertyname/$PropertyName','api/accommodation/accommodationbypropertyname.php');
+$router->get('/accommodationbykey','api/accommodation/accommodationbykey.php');
+$router->get('/accommodationbyaddress','api/accommodation/accommodationbylocation.php');
+$router->get('/accommodationbypropertyname','api/accommodation/accommodationbypropertyname.php');
 
 
 ?>
--- router.php
+++ router.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 class Router {
 
     public $handled = false;
@@ -17,7 +17,34 @@ class Router {
 
         $uri = $_SERVER['REQUEST_URI'];
         $route = "/uj-accommodation-api" . $route;
-    
+
+        if(isset($_GET['key'])){
+            if($_GET['key'] != ''){
+
+                $view = "api/accommodation/accommodationbykey.php";
+                $route = $uri;
+           }
+        }
+
+
+        if(isset($_GET['address'])){
+            if($_GET['address'] != ''){
+
+                $view = "api/accommodation/accommodationbylocation.php";
+                $route = $uri;
+            }
+        }
+
+        if(isset($_GET['PropertyName'])){
+            if($_GET['PropertyName'] != ''){
+
+                $view = "api/accommodation/accommodationbypropertyname.php";
+                $route = $uri;
+               }
+        
+        }
+
+
 
         if($uri === $route){
             $this->handled = true;
